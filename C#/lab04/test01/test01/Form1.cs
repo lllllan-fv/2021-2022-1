00621babc5a4daf2244a16cee66a4971a09f8b5b
lllllan <342310798@qq.com>
2021-09-28 15:08:44 +0800

    add C# (unfinished)lab04

---
+++ C#/lab04/test01/test01/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test01 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            listBox1.Items.Add("排骨炖白菜");
+            listBox1.Items.Add("红烧狮子头");
+            listBox1.Items.Add("上海醉蟹");
+            listBox1.Items.Add("海带汤");
+            listBox1.Items.Add("山峦猪脚");
+            listBox1.Items.Add("鸡蛋汤");
+            listBox1.Items.Add("炸猪排");
+
+            enbutton();
+            
+        }
+
+        private void enbutton() {
+
+            if (listBox1.Items.Count == 0) {
+                button1.Enabled = false;
+            } else {
+                button1.Enabled = true;
+            }
+
+            if (listBox2.Items.Count == 0) {
+                button2.Enabled = false;
+            } else {
+                button2.Enabled = true;
+            }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (listBox1.SelectedIndex >= 0) {
+                listBox2.Items.Add(listBox1.SelectedItem);
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+            enbutton();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            if (listBox2.SelectedIndex >= 0) {
+                listBox1.Items.Add(listBox2.SelectedItem);
+                listBox2.Items.RemoveAt(listBox2.SelectedIndex);
+            }
+            enbutton();
+        }
+    }
+}
---
+++ C#/lab04/test02/test02/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test02 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e) {
+            button_Click("6");
+        }
+
+        private void button10_Click(object sender, EventArgs e) {
+            button_Click("-");
+        }
+
+        private void button8_Click(object sender, EventArgs e) {
+            button_Click("5");
+        }
+
+        private void button_Click(String s) {
+            if (textBox1.Text == "") {
+                textBox1.Text = s;
+                return;
+            }
+
+            String text = "";
+
+            char last = textBox1.Text[textBox1.Text.Length - 1];
+            if (s == "+" || s == "-" || s == "*" || s == "/") {
+                if (last == '+' || last == '-' || last == '*' || last == '/') {
+                    text = textBox1.Text.Substring(0, textBox1.Text.Length - 1) + s;
+                } else {
+                    text = textBox1.Text + s;
+                }
+            } else if (s == ".") {
+                if (textBox1.Text[textBox1.Text.Length - 1] < '0' || textBox1.Text[textBox1.Text.Length - 1] > '9') {
+                    text = textBox1.Text;
+                } else {
+                    text = textBox1.Text + s;
+                }
+            } else {
+                text = textBox1.Text + s;
+            }     
+       
+            textBox1.Text = text;
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            button_Click("1");
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            button_Click("2");
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+            button_Click("3");
+        }
+
+        private void button7_Click(object sender, EventArgs e) {
+            button_Click("4");
+        }
+
+        private void button11_Click(object sender, EventArgs e) {
+            button_Click("7");
+        }
+
+        private void button12_Click(object sender, EventArgs e) {
+            button_Click("8");
+        }
+
+        private void button13_Click(object sender, EventArgs e) {
+            button_Click("9");
+        }
+
+        private void button6_Click(object sender, EventArgs e) {
+            button_Click("+");
+        }
+
+        private void button14_Click(object sender, EventArgs e) {
+            button_Click("*");
+        }
+
+        private void button15_Click(object sender, EventArgs e) {
+            button_Click("0");
+        }
+
+        private void button17_Click(object sender, EventArgs e) {
+            button_Click(".");
+        }
+
+        private void button18_Click(object sender, EventArgs e) {
+            button_Click("/");
+        }
+
+        private void button16_Click(object sender, EventArgs e) {
+            DataTable dt = new DataTable();
+            textBox1.Text = dt.Compute(textBox1.Text, "").ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            textBox1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (textBox1.Text.Length > 0) {
+                textBox1.Text = textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+            }                
+        }
+
+        private void button10_Click_1(object sender, EventArgs e) {
+            button_Click("-");
+        }
+    }
+}
