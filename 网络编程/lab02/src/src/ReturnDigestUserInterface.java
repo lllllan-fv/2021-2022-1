60c446c05de1458ab6c81be431f40470a8634815
lllllan <342310798@qq.com>
2021-10-20 10:14:15 +0800

    add 网络编程-lab03(un)

---
+++ 网络编程/lab02/src/src/ReturnDigestUserInterface.java
@@ -0,0 +1,22 @@
+import javax.xml.bind.DatatypeConverter;
+
+public class ReturnDigestUserInterface {
+
+    private static final String[] FILES = new String[]{
+            "1.txt", "2.txt", "3.txt", "4.txt"
+    };
+
+    public static void main(String[] args) {
+        for (String filename : FILES) {
+            // 计算摘要
+            ReturnDigest dr = new ReturnDigest(filename);
+            dr.start();
+            //现在显示结果
+            StringBuilder result = new StringBuilder(filename);
+            result.append(": ");
+            byte[] digest = dr.getDigest();
+            result.append(DatatypeConverter.printHexBinary(digest));
+            System.out.println(result);
+        }
+    }
+}
