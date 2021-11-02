168e3a65a8822d0b4a14b4f11c40ccd8f7675522
lllllan <342310798@qq.com>
2021-11-02 19:16:33 +0800

    add .

---
+++ C#/lab07/test01/test01/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace test01 {
+    public partial class Form1 : Form {
+        private int cnt = 0;
+        private Boolean stopFlag = false;
+        SynchronizationContext m_SyncContext = null;
+
+        public Form1() {
+            InitializeComponent();
+            m_SyncContext = SynchronizationContext.Current;
+        }
+
+        public void run() {
+            int id = ++cnt;
+            for (int i = 1; i <= 20; ++i) {
+                while (stopFlag) ;
+                m_SyncContext.Post(SetTextSafePost, "线程 " + id + ": " + i);
+                Thread.Sleep(300);
+            }
+        }
+
+        private void SetTextSafePost(object text) {
+            this.richTextBox1.Text = this.richTextBox1.Text + "\r\n" + text.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            Thread t1 = new Thread(new ThreadStart(run));
+            t1.Start();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            stopFlag = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            stopFlag = false;
+        }
+    }
+}
---
+++ C#/lab07/test03/test03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace test03 {
+    class Program { 
+
+        public void run() { 
+            for (int i = 1; i <= 20; ++i) {
+                Console.WriteLine("线程" + Thread.CurrentThread.Name + " " + i);
+                Thread.Sleep(200);
+            }
+        }
+
+        static void Main(string[] args) {
+            for (int i = 1; i <= 3; ++i) {
+
+                Program p = new Program();
+                Thread t = new Thread(new ThreadStart(p.run));
+                t.Name = i.ToString();
+
+                if (i == 1) t.Priority = ThreadPriority.Lowest;
+                else if (i == 2) t.Priority = ThreadPriority.Normal;
+                else t.Priority = ThreadPriority.Highest;
+
+                t.Start();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
