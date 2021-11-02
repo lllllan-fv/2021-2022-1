85be080d91c23b2d839be396a405d49bc9afcecb
lllllan <342310798@qq.com>
2021-11-02 21:28:09 +0800

    add C#-lab07

---
+++ C#/lab07/软工192-苏桐渤-实验七/test02/test02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace test02
+{
+    class Program
+    {
+
+        public void run()
+        {
+            Console.WriteLine("线程标识符：" + Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("线程名：" + Thread.CurrentThread.Name);
+            Console.WriteLine("线程状态：" + Thread.CurrentThread.ThreadState);
+            Console.WriteLine("线程优先级：" + Thread.CurrentThread.Priority); 
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Thread t = new Thread(new ThreadStart(p.run));
+            t.Name = "wahaha";
+
+            t.Start();
+        }
+    }
+}
---
+++ C#/lab07/软工192-苏桐渤-实验七/test04/test04/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace test04
+{
+    class Program
+    {
+
+        List<ManualResetEvent> manualEvents = new List<ManualResetEvent>();
+        public void threadA()
+        {
+            for (int i = 1; i <= 10; ++i)
+            {
+
+                int sz = new Random().Next(1, 5);
+                for (int j = 1; j <= sz; ++j)
+                {
+                    ManualResetEvent mre = new ManualResetEvent(false);
+                    manualEvents.Add(mre);
+                    ThreadPool.QueueUserWorkItem(threadB, mre);
+                }
+
+                WaitHandle.WaitAll(manualEvents.ToArray());
+                Console.WriteLine("ThreadA Finished!" + i);
+            }
+        }
+
+        private void threadB(object state)
+        {
+            Thread.Sleep(new Random().Next(100, 300));
+
+            ManualResetEvent mre = (ManualResetEvent) state;
+            mre.Set();
+            Console.WriteLine("子线程" + new Random().Next(100));
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Thread t = new Thread(new ThreadStart(p.threadA));
+            t.Start();
+        }
+    }
+}
