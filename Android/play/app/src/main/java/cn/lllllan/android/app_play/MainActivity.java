5ee8b6683f07debb87107734799eb8242e00ec02
lllllan <342310798@qq.com>
2021-11-25 09:31:40 +0800

    android实验进度：能过正常播放音乐

---
+++ Android/play/app/src/main/java/cn/lllllan/android/app_play/MainActivity.java
@@ -0,0 +1,91 @@
+package cn.lllllan.android.app_play;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        videoView = (VideoView) findViewById(R.id.main_video_view);
+        Button play = (Button) findViewById(R.id.main_button_play);
+        Button pause = (Button) findViewById(R.id.main_button_pause);
+        Button replay = (Button) findViewById(R.id.main_button_replay);
+
+        play.setOnClickListener(this);
+        pause.setOnClickListener(this);
+        replay.setOnClickListener(this);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        } else {
+            initVideoPath(); // 初始化MediaPlayer
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initVideoPath();
+                } else {
+                    Toast.makeText(this, "拒绝权限将无法使用程序", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
+
+    private void initVideoPath() {
+        File file = new File(Environment.getExternalStorageDirectory(), "com.netease.cloudmusic/music/爱情陷阱.flac");
+        videoView.setVideoPath(file.getPath()); // 指定视频文件的路径
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.main_button_play:
+                if (!videoView.isPlaying()) {
+                    videoView.start(); // 开始播放
+                }
+                break;
+            case R.id.main_button_pause:
+                if (videoView.isPlaying()) {
+                    videoView.pause(); // 暂停播放
+                }
+                break;
+            case R.id.main_button_replay:
+                if (videoView.isPlaying()) {
+                    videoView.resume(); // 重新播放
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (videoView != null) {
+            videoView.suspend();
+        }
+    }
+}
